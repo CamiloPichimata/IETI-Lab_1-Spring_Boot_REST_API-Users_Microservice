a9e560a048f4d75d48301a7385127f2a5a1063fa
Camilo Pichimata <camilo.pichimata@mail.escuelaing.edu.co>
2022-09-01 15:20:24 -0500

    Se realizaron modificaciones para la ejecución desde la línea de comandos

--- src/test/java/com/escuelaing/ieti/dto/DtoApplicationTests.java
+++ src/test/java/com/escuelaing/ieti/dto/DtoApplicationTests.java
@@ -3,10 +3,10 @@ package com.escuelaing.ieti.dto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DtoApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
